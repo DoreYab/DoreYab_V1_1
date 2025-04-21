dc29686aede250eca9b719644ee799656fad79e9
سهیل ایمانی اختیار <soheil81imani81@gmail.com>
2025-04-20 19:51:42 -0800

    Course Category DTO Model

---
+++ DoreYab_V1_1/DY.Application.Contract/CourseCategory/CourseCategoryViewModel.cs
@@ -0,0 +1,23 @@
+﻿using _0_Framework.Domain;
+
+namespace DY.Application.Contract.CourseCategory
+{
+    public class CourseCategoryViewModel : EntityBase
+    {
+        public string Title { get; set; }
+        public string? ShortDescription { get; set; }
+        public long CourseCount { get; set; }
+        public bool IsDeleted { get; set; }
+
+        public CourseCategoryViewModel(string title, string? shortDescription, long courseCount)
+        {
+            Title = title;
+            ShortDescription = shortDescription;
+            CourseCount = courseCount;
+            IsDeleted = false;
+            CreationDate = DateTime.UtcNow;
+        }
+
+        public CourseCategoryViewModel() { }
+    }
+}
