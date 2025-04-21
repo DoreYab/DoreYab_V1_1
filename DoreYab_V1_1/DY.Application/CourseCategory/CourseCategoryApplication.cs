627478542d1f530ddbeab94d5e3926faffd66101
سهیل ایمانی اختیار <soheil81imani81@gmail.com>
2025-04-20 19:54:24 -0800

    Application Layer

---
+++ DoreYab_V1_1/DY.Application/CourseCategory/CourseCategoryApplication.cs
@@ -0,0 +1,38 @@
+﻿using DY.Application.Contract.CourseCategory;
+using DY.Domain.CourseCategoryAgg;
+using System.Reflection.Metadata.Ecma335;
+
+namespace DY.Application.CourseCategory
+{
+    public class CourseCategoryApplication : ICourseCategoryApplication
+    {
+        private readonly ICourseCategoryRepository _categoryRepository;
+
+        public CourseCategoryApplication(ICourseCategoryRepository categoryRepository)
+        {
+            _categoryRepository = categoryRepository;
+        }
+
+        public List<CourseCategoryViewModel> GetAll()
+        {
+            var courseCategories = _categoryRepository.GetAll();
+            var result = new List<CourseCategoryViewModel>();
+
+            foreach (var courseCategory in courseCategories)
+            {
+                result.Add(new CourseCategoryViewModel
+                {
+                    Id = courseCategory.Id,
+                    Title = courseCategory.Title,
+                    IsDeleted = courseCategory.IsDeleted,
+                    ShortDescription = courseCategory.ShortDescription,
+                    CreationDate = courseCategory.CreationDate,
+                });
+            }
+            return result;
+        }
+        
+        
+       
+    }
+}
