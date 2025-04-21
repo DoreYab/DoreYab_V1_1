af3abbe83affc90dd002842b7b257cf5a35ea573
سهیل ایمانی اختیار <soheil81imani81@gmail.com>
2025-04-20 19:52:34 -0800

    Domain layer with Category Entity and Repository

---
+++ DoreYab_V1_1/DY.Domain/CourseCategoryAgg/ICourseCategoryRepository.cs
@@ -0,0 +1,10 @@
+﻿using Domain.CourseCategory;
+
+namespace DY.Domain.CourseCategoryAgg
+{
+    public interface ICourseCategoryRepository
+    {
+        void Create(CourseCategory entuty);
+        List<CourseCategory> GetAll();
+    }
+}
