d7274a0c7935b1e9311132d0d341bed9e81b2174
mohammad mehdi khodabande <mmahdikhodabande@gmail.com>
2025-04-24 10:14:14 +0330

    Add program.cs

--- DoreYab_V1_1/DY.Presentation/Program.cs
+++ DoreYab_V1_1/DY.Presentation/Program.cs
@@ -49,6 +49,10 @@ namespace DY.Presentation
             app.UseAuthorization();
 
             app.MapControllerRoute(
+                name: "areas",
+                pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+
+            app.MapControllerRoute(
                 name: "default",
                 pattern: "{controller=Home}/{action=Index}/{id?}");
 
