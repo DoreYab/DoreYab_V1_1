d4bee9de4b1daf1ec69f4ed1ceac1cb3bb76a2e9
سهیل ایمانی اختیار <soheil81imani81@gmail.com>
2025-04-29 23:59:15 -0800

    Add Edit Action

--- DoreYab_V1_1/DY.Presentation/Areas/Admin/Controllers/CategoryController.cs
+++ DoreYab_V1_1/DY.Presentation/Areas/Admin/Controllers/CategoryController.cs
@@ -6,7 +6,10 @@ namespace DY.Presentation.Area.Admin.Controllers
     [Area("Admin")]
     public class CategoryController : Controller
     {
+       
+        public RenameCourseCategory RenameCategory { get; set; }
         public List<CourseCategoryViewModel> CourseCategories { get; set; }
+        
         private readonly ICourseCategoryApplication _categoryApplication;
 
         public CategoryController(ICourseCategoryApplication categoryApplication)
@@ -31,7 +34,25 @@ namespace DY.Presentation.Area.Admin.Controllers
         public IActionResult Create(CreateCourseCategory command)
         {
             _categoryApplication.Create(command);
-            return RedirectToAction("./List");
+            return RedirectToAction("List");
         }
+
+        [HttpGet("Edit/{id:long}")]
+        public IActionResult Edit(long id)
+        {
+            var courseCategory = _categoryApplication.Get(id);
+            return View(courseCategory);
+        }
+
+        [HttpPost("Edit")]
+        public IActionResult Edit(RenameCourseCategory model)
+        {
+            if(!ModelState.IsValid)
+                return View(model);
+
+            _categoryApplication.Rename(model);
+            return RedirectToAction("List");
+        }
+        
     }
 }
