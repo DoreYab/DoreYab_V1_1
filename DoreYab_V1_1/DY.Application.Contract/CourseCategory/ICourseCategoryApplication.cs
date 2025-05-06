632a09e081e7fcc10c4249e30b7b0f165179b15a
سهیل ایمانی اختیار <soheil81imani81@gmail.com>
2025-05-06 00:05:36 -0800

    add Activate method

--- DoreYab_V1_1/DY.Application.Contract/CourseCategory/ICourseCategoryApplication.cs
+++ DoreYab_V1_1/DY.Application.Contract/CourseCategory/ICourseCategoryApplication.cs
@@ -9,6 +9,6 @@ namespace DY.Application.Contract.CourseCategory
         void Rename(RenameCourseCategory command);
         RenameCourseCategory Get(long id);
         void Remove(long id);
-        void Activate(long id);
+        void Activate(long id); 
     }
 }
