8fd639658ef3a4fa06af49752afb24295de6c83c
سهیل ایمانی اختیار <soheil81imani81@gmail.com>
2025-05-09 22:34:10 -0800

    Move class from interface

---
+++ DoreYab_V1_1/DY.Domain/Services/CourseCategoryalidatorServices.cs
@@ -0,0 +1,20 @@
+﻿using DY.Domain.CourseCategoryAgg;
+
+namespace DY.Domain.Services
+{
+    public class CourseCategoryalidatorServices : ICourseCategoryValidatorServices
+    {
+        private readonly ICourseCategoryRepository _categoryRepository;
+
+        public CourseCategoryalidatorServices(ICourseCategoryRepository categoryRepository)
+        {
+            _categoryRepository = categoryRepository;
+        }
+
+        public void CheckThisRecordAlreadyExsist(string title)
+        {
+            if (_categoryRepository.Exsist(title))
+                throw new Exception();
+        }
+    }
+}
