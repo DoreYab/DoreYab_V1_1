f95f4f9205aad48a9633458b04e2dd72aa728ebc
mohammad mehdi khodabande <mmahdikhodabande@gmail.com>
2025-05-18 16:41:35 +0330

    Add EditEntity migration for Courses table updates
    This commit introduces a new migration class `EditEntity` that renames the `Desctiption` column to `Description` and alters the `CourseUrl` column to be non-nullable in the `Courses` table. The `BuildTargetModel` method in `EditEntity.Designer.cs` defines the structure of the `Course` and `CourseCategory` entities, including their properties, keys, and relationships. The `DoreYab_ContextModelSnapshot.cs` file is also updated to reflect these changes.

---
+++ DoreYab_V1_1/DY.Inferastructure.EfCore/Migrations/20250518120321_Edit Entity .cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DY.Inferastructure.EfCore.Migrations
+{
+    /// <inheritdoc />
+    public partial class EditEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Desctiption",
+                table: "Courses",
+                newName: "Description");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CourseUrl",
+                table: "Courses",
+                type: "varchar(1000)",
+                unicode: false,
+                maxLength: 1000,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "varchar(1000)",
+                oldUnicode: false,
+                oldMaxLength: 1000,
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Description",
+                table: "Courses",
+                newName: "Desctiption");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CourseUrl",
+                table: "Courses",
+                type: "varchar(1000)",
+                unicode: false,
+                maxLength: 1000,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(1000)",
+                oldUnicode: false,
+                oldMaxLength: 1000);
+        }
+    }
+}
