0a071d23d4af3e3584ca344028d479780e304be9
mohammad mehdi khodabande <mmahdikhodabande@gmail.com>
2025-05-25 16:12:22 +0330

    Add identity management entities and fix typos
    This commit introduces new entities for user management, including `AspNetUsers` and `AspNetRoles`, along with their claims and tokens. Migration files have been updated to reflect these changes and establish necessary foreign key relationships. Additionally, a typo in the `Courses` table's `Description` field has been corrected from "Desctiption" to "Description". The database schema is now enhanced to support user authentication and authorization features.

--- DoreYab_V1_1/DY.Inferastructure.EfCore/Migrations/20250525115444_InitialCreate.Designer.cs
+++ DoreYab_V1_1/DY.Inferastructure.EfCore/Migrations/20250525115444_InitialCreate.Designer.cs
@@ -4,6 +4,7 @@ using DY.Inferastructure.EfCore.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DY.Inferastructure.EfCore.Migrations
 {
     [DbContext(typeof(DoreYab_Context))]
-    partial class DoreYab_ContextModelSnapshot : ModelSnapshot
+    [Migration("20250525115444_InitialCreate")]
+    partial class InitialCreate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
