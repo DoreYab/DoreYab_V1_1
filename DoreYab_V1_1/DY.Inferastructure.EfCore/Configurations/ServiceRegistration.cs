c060b9e26beabd99e2b1ec3e2e8d415f540ceb4a
سهیل ایمانی اختیار <soheil81imani81@gmail.com>
2025-05-24 20:17:57 -0800

    add class and config Identity

---
+++ DoreYab_V1_1/DY.Inferastructure.EfCore/Configurations/ServiceRegistration.cs
@@ -0,0 +1,61 @@
+﻿using DY.Domain.Identity;
+using DY.Inferastructure.EfCore.Data;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DY.Inferastructure.EfCore.Configurations
+{
+    public static class ServiceRegistration
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<DoreYab_Context>(options =>
+                options.UseSqlServer(configuration.GetConnectionString("DoreYab_V1_1")));
+
+            services.AddIdentity<ApplicationUser, IdentityRole>()
+                .AddEntityFrameworkStores<DoreYab_Context>()
+                .AddDefaultTokenProviders();
+
+            services.Configure<IdentityOptions>(options =>
+            {
+                options.Password.RequireDigit = false;
+                options.Password.RequiredLength = 6;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireUppercase = false;
+            });
+
+            var jwtSettings = configuration.GetSection("JwtSettings");
+            var secret = jwtSettings["Secret"];
+
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = jwtSettings["Issuer"],
+                    ValidAudience = jwtSettings["Audience"],
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secret))
+                };
+            });
+
+            return services;
+        }
+
+    }
+}
