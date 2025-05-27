a5c29ae30dc07f79ea2d81aac41616dd19c8d134
سهیل ایمانی اختیار <soheil81imani81@gmail.com>
2025-05-27 15:05:10 -0800

    design Login ViewModel

--- DoreYab_V1_1/DY.Presentation/Models/Account/LoginViewModel.cs
+++ DoreYab_V1_1/DY.Presentation/Models/Account/LoginViewModel.cs
@@ -1,6 +1,13 @@
-﻿namespace DY.Presentation.Models.Account
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DY.Presentation.Models.Account
 {
     public class LoginViewModel
     {
+        [Required, EmailAddress, Display(Name ="ایمیل")]
+        public string Email { get; set; }
+
+        [Required, DataType(DataType.Password), Display(Name ="رمز عبور")]
+        public string Password { get; set; }
     }
 }
