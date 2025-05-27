b58dbb3af2ab7397878b58d47840791d1cd9c8f8
سهیل ایمانی اختیار <soheil81imani81@gmail.com>
2025-05-26 23:11:28 -0800

    impliment interface

--- DoreYab_V1_1/DY.Inferastructure.EfCore/Identity/AuthService.cs
+++ DoreYab_V1_1/DY.Inferastructure.EfCore/Identity/AuthService.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using DY.Application.Authentication.Models;
+using DY.Application.Common.Interfaces;
+using DY.Domain.Identity;
+using Microsoft.AspNetCore.Identity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +10,63 @@ using System.Threading.Tasks;
 
 namespace DY.Inferastructure.EfCore.Identity
 {
-    internal class AuthService
+    public class AuthService : IAuthService
     {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly IJwtTokenGenerator _jwtTokenGenerator;
+
+        public AuthService(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager, IJwtTokenGenerator jwtTokenGenerator)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _jwtTokenGenerator = jwtTokenGenerator;
+        }
+
+        public async Task<AuthResponse> RegisterAsync(RegisterRequest request)
+        {
+            var user = new ApplicationUser
+            {
+                UserName = request.Email,
+                Email = request.Email,
+                FullName = request.FullName
+            };
+
+            var result = await _userManager.CreateAsync(user, request.Password);
+
+            if (!result.Succeeded)
+                throw new Exception(string.Join(" | ", result.Errors.Select(e => e.Description)));
+
+            var token = _jwtTokenGenerator.GenerateToken(user);
+
+            return new AuthResponse
+            {
+                UserId = user.Id,
+                Email = user.Email!,
+                FullName = user.FullName!,
+                Token = token
+            };
+        }
+
+        public async Task<AuthResponse> LoginAsync(LoginRequest request)
+        {
+            var user = await _userManager.FindByEmailAsync(request.Email)
+                       ?? throw new Exception("Invalid credentials.");
+
+            var result = await _signInManager.CheckPasswordSignInAsync(user, request.Password, false);
+
+            if (!result.Succeeded)
+                throw new Exception("Invalid credentials.");
+
+            var token = _jwtTokenGenerator.GenerateToken(user);
+
+            return new AuthResponse
+            {
+                UserId = user.Id,
+                Email = user.Email!,
+                FullName = user.FullName!,
+                Token = token
+            };
+        }
     }
 }
