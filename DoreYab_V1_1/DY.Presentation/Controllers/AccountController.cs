c44b39b7d2995b03618fe1b65e4c6808cc9aab27
سهیل ایمانی اختیار <soheil81imani81@gmail.com>
2025-05-27 15:23:27 -0800

    design and create code for Register user

---
+++ DoreYab_V1_1/DY.Presentation/Controllers/AccountController.cs
@@ -0,0 +1,49 @@
+﻿using DY.Application.Authentication.Models;
+using DY.Application.Common.Interfaces;
+using DY.Presentation.Models.Account;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DY.Presentation.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly IAuthService _authService;
+
+        public AccountController(IAuthService authService)
+        {
+            _authService = authService;
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel registerView)
+        {
+            if (!ModelState.IsValid)
+                return View(registerView);
+
+            try
+            {
+                var requrst = new RegisterRequest
+                {
+                    FullName = registerView.FullName,
+                    Email = registerView.Email,
+                    Password = registerView.Password,
+                };
+
+                var result = await _authService.RegisterAsync(requrst);
+                return RedirectToAction("Index", "Home");
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", ex.Message);
+                return View(registerView);
+            }
+        }
+    }
+}
+
