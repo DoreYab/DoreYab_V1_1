a4a443ad1c7f0db0149fcd617f32710a3c93b5ad
سهیل ایمانی اختیار <soheil81imani81@gmail.com>
2025-05-26 22:46:30 -0800

    create interfaces

--- DoreYab_V1_1/DY.Application/Common/Interfaces/IJwtTokenGenerator.cs
+++ DoreYab_V1_1/DY.Application/Common/Interfaces/IJwtTokenGenerator.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DY.Domain.Identity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,8 @@ using System.Threading.Tasks;
 
 namespace DY.Application.Common.Interfaces
 {
-    internal class IJwtTokenGenerator
+    public class IJwtTokenGenerator
     {
+        string GenerateToken(ApplicationUser user);
     }
 }
