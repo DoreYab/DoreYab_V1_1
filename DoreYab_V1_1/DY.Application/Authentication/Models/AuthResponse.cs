780b5a534aae6455c4eb0317e75597c1bef7a20d
سهیل ایمانی اختیار <soheil81imani81@gmail.com>
2025-05-26 23:11:15 -0800

    change praperty in class

--- DoreYab_V1_1/DY.Application/Authentication/Models/AuthResponse.cs
+++ DoreYab_V1_1/DY.Application/Authentication/Models/AuthResponse.cs
@@ -2,9 +2,9 @@
 {
     public class AuthResponse
     {
-        public string USerId { get; set; }
+        public string UserId { get; set; }
         public string Email { get; set; }
         public string FullName { get; set; }
-        public string Tocken { get; set; }
+        public string Token { get; set; }
     }
 }
