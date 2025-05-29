3d9449bd13c3e932e1e712fc1f1b3be20f7d5840
سهیل ایمانی اختیار <soheil81imani81@gmail.com>
2025-05-29 10:05:43 -0800

    add confirm Password to viewModel

--- DoreYab_V1_1/DY.Presentation/Models/Account/RegisterViewModel.cs
+++ DoreYab_V1_1/DY.Presentation/Models/Account/RegisterViewModel.cs
@@ -11,6 +11,12 @@ namespace DY.Presentation.Models.Account
         public string Email { get; set; }
 
         [Required, DataType(DataType.Password), Display(Name ="رمز عبور")]
+        
         public string Password { get; set; }
+
+        [Required, DataType(DataType.Password), Display(Name = "تأیید رمز عبور")]
+        [Compare("Password", ErrorMessage = "رمز عبور و تأیید آن یکسان نیستند.")]
+        public string ConfirmPassword { get; set; }
+
     }
 }
