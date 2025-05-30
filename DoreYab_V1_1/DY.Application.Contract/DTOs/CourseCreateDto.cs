96559bfd15dc763e044e0fe5444de3d5d1143523
mohammad mehdi khodabande <mmahdikhodabande@gmail.com>
2025-05-30 16:30:09 +0330

    Add CourseCreateDto and CourseUpdateDto classes
    Introduced two new DTO classes in the DY.Application.Contract.DTOs namespace:
    - `CourseCreateDto` for creating new courses with properties like Title, Price, Description, and more.
    - `CourseUpdateDto` for updating existing courses, including an Id property for identification.
    Both classes support nullable types for optional properties.

---
+++ DoreYab_V1_1/DY.Application.Contract/DTOs/CourseCreateDto.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DY.Application.Contract.DTOs
+{
+    public class CourseCreateDto
+    {
+        public string Title { get; set; }
+        public decimal? Price { get; set; }
+        public string? Description { get; set; }
+        public string CourseUrl { get; set; }
+        public string SiteSource { get; set; }
+        public string Slug { get; set; }
+        public string ImageUrl { get; set; } // از فایل آپلود شده میاد
+        public bool IsFree { get; set; }
+        public bool IsFinished { get; set; }
+        public string? MetaTitle { get; set; }
+        public string? MetaDescription { get; set; }
+        public string? MetaKeyword { get; set; }
+        public long SelectedCategoryId { get; set; }
+    }
+}
---
+++ DoreYab_V1_1/DY.Application.Contract/DTOs/CourseUpdateDto.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DY.Application.Contract.DTOs
+{
+    public class CourseUpdateDto
+    {
+        public long Id { get; set; }
+        public string Title { get; set; }
+        public decimal? Price { get; set; }
+        public string? Description { get; set; }
+        public string CourseUrl { get; set; }
+        public string SiteSource { get; set; }
+        public string Slug { get; set; }
+        public string? ImageUrl { get; set; } 
+        public bool IsFree { get; set; }
+        public bool IsFinished { get; set; }
+        public string? MetaTitle { get; set; }
+        public string? MetaDescription { get; set; }
+        public string? MetaKeyword { get; set; }
+        public long SelectedCategoryId { get; set; }
+    }
+}
