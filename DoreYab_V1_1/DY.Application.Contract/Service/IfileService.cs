d5a719f4d0d0c2a775f199fcb1b2260074f09c1c
mohammad mehdi khodabande <mmahdikhodabande@gmail.com>
2025-05-30 16:31:02 +0330

    Add IfileService interface for file management
    Introduces the `IfileService` interface in the
    `DY.Application.Contract.Service` namespace. This
    interface includes methods for saving files and
    thumbnails asynchronously, as well as deleting files.
    Necessary namespaces are also imported to support
    the interface functionality.

---
+++ DoreYab_V1_1/DY.Application.Contract/Service/IfileService.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DY.Application.Contract.Service
+{
+    public interface IfileService
+    {   
+        Task<string> SaveFileAsync(IFormFile file, string folderName);
+        Task<string> SaveThumbnailAsync(IFormFile file, string folderName, int width = 300, int height = 200);
+        void DeleteFile(string filePath);
+    }
+}
