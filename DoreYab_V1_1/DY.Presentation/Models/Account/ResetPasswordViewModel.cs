fbeeba061363c9d8779b686ca80251a2c1043d16
سهیل ایمانی اختیار <soheil81imani81@gmail.com>
2025-06-01 12:27:04 -0800

    add email property

--- DoreYab_V1_1/DY.Presentation/Models/Account/ResetPasswordViewModel.cs
+++ DoreYab_V1_1/DY.Presentation/Models/Account/ResetPasswordViewModel.cs
@@ -1,6 +1,12 @@
-﻿namespace DY.Presentation.Models.Account
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DY.Presentation.Models.Account
 {
     public class ResetPasswordViewModel
     {
+        [Required(ErrorMessage ="وارد کردن ایمیل الزامیست")]
+        [EmailAddress(ErrorMessage ="ایمیل معتبر نیست")]
+        public string Email { get; set; }
+
     }
 }
